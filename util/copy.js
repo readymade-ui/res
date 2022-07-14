9a4740e10686d2e69c8b24f3e65d94be909c49d8
Stephen Belovarich <steveblue@gmail.com>
2022-07-14 10:36:39 -0700

    fix: package format
    feat: bun

--- util/copy.js
+++ util/copy.js
@@ -1,5 +1,5 @@
-const { readFileSync, writeFileSync } = require("fs");
-const { join } = require("path");
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
 
 const manifestFile = readFileSync("package.json", "utf-8");
 const manifest = JSON.parse(manifestFile);
